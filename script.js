c7607fd034cee4490864ced8ad4236e1cb6bddff
Alexandra <alexmiriamd@gmail.com>
2025-04-16 13:48:52 -0600

    obstacle movement, refining joey's jump

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startGame);
 
 function startGame(e) {
-    console.log("button was pressed");
     startScreen.classList.add("fade-out");
     
     setTimeout(()=> {
@@ -20,11 +19,25 @@ function startGame(e) {
 const joey = document.querySelector(".joey");
 
 document.addEventListener("keydown", (e) => {
-    console.log("key was pressed");
     if(e.code === "Space" && !joey.classList.contains("jump")) {
         joey.classList.add("jump");
         setTimeout(() => {
             joey.classList.remove("jump");
-        }, 300);
+        }, 600);
     }
 });
+
+//obstacle mechanics
+const obstacle = document.querySelector(".obstacle");
+let obstacleX = window.innerWidth; //starting off screen
+
+function moveObstacle() {
+    obstacleX -= 2; //moving left 5px every frame
+    obstacle.style.left = obstacleX + "px";
+
+    if (obstacleX < -50) {
+        obstacleX = window.innerWidth; //reset to starting position
+    }
+    requestAnimationFrame(moveObstacle); //keep moving the obstacle
+}
+moveObstacle(); //start the animation!
